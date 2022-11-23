2a6d2dbd91438b650c2321bbdfb6692b08e72a13
anarmh <anarmh@code.edu.az>
2022-11-23 08:56:42 -0800

    Add factorial with exception

---
+++ Calculate factorial  with exception/Factorial/Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿
+CalculateFactorial(-5);
+
+
+
+ static void CalculateFactorial(int numb)
+{
+    int mult = 1;
+    try
+    {
+        if (numb == 0)
+        {
+            Console.WriteLine("0 has no Factorial");
+        }
+        else if (numb < 0)
+        {
+            throw new ArgumentException();
+        }
+        for (int i = numb; i > 1; i--)
+        {
+            mult *= i;
+            Console.WriteLine(mult);
+        }
+        
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+   
+    
+}
